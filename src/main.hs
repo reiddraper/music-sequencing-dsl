e22569d2c37dbdc3490918c54717185124304074
Reid Draper <reiddraper@gmail.com>
2022-07-25 09:39:02 -0500

    It works!

--- src/main.hs
+++ src/main.hs
@@ -2,10 +2,15 @@ module Main (main) where
 
 import qualified Codec.Midi as Midi
 import Control.Monad (forM_)
+import Data.Interval (Extended (Finite), Interval, (<=..<), (<=..<=))
 import qualified Data.Map as Map
 import qualified Data.Map.Delta as Delta
+import Data.Maybe (fromMaybe)
 import Data.Ratio ((%))
+import Data.SegmentTree (queryTree)
+import qualified Data.SegmentTree as SegmentTree
 import Data.Tuple.Extra (second)
+import qualified Debug.Trace as Debug
 
 rationalRemainder :: Rational -> Rational -> Rational
 rationalRemainder x y =
@@ -20,18 +25,13 @@ type Velocity = Integer
 
 -- Do we need this?
 data MidiNote = MidiNote
-  { note :: Note,
-    velocity :: Velocity,
-    channel :: Channel
+  { channel :: Channel,
+    note :: Note,
+    velocity :: Velocity
   }
   deriving (Eq, Ord, Show)
 
-type MidiSound = Map.Map Channel (Map.Map Note Velocity)
-
--- newtype Audio = Audio (Maybe Sound) deriving (Show, Semigroup)
-
--- instance Monoid Audio where
---   mempty = Audio Nothing
+type MidiSound = Map.Map (Channel, Note) Velocity
 
 data NoteEvent
   = NoteOn
@@ -45,18 +45,59 @@ sample rate end composition = [(t, composition t) | t <- [0, rate .. (end - rate
 
 samplesToEvents ::
   [(Rational, MidiSound)] ->
-  [(Rational, (Note, NoteEvent))]
-samplesToEvents samples = reverse $ fst $ foldl go ([], Map.empty :: MidiSound) samples
+  [(Rational, (Channel, Note, Velocity, NoteEvent))]
+samplesToEvents samples = fst $ foldl go ([], Map.empty :: MidiSound) samples
   where
-    go (events, prevMidiSound) (when, midiSound) = (newEvents when prevMidiSound midiSound : events, midiSound)
-    newEvents _when _prevMidiSound _midiSound =
-      let diff = Delta.diff _prevMidiSound _midiSound
-       in undefined
+    go (events, prevMidiSound) (when, midiSound) = (events ++ newEvents when prevMidiSound midiSound, midiSound)
+
+    newEvents when prevMidiSound midiSound =
+      let diff = Delta.diff prevMidiSound midiSound
+          old = Delta.toOld diff
+          new = Delta.toNew diff
+          different = Delta.toDelta diff
+       in [(when, (channel, note, velocity, NoteOff)) | ((channel, note), velocity) <- Map.toList old]
+            ++ [(when, (channel, note, velocity, NoteOn)) | ((channel, note), velocity) <- Map.toList new]
+            ++ [(when, (channel, note, newVel, NoteOn)) | ((channel, note), Delta.DeltaUnit _oldVel newVel) <- Map.toList different]
+
+type NoteStartMap = Map.Map (Channel, Note) (Rational, Velocity)
+
+trackAbsoluteTime :: Midi.Track Midi.Ticks -> Midi.Track Midi.Ticks
+trackAbsoluteTime track = reverse $ snd $ foldl trackAbsoluteTimeFolder (0, []) track
+  where
+    trackAbsoluteTimeFolder (absoluteTime, events) (diff, event) =
+      (absoluteTime + diff, (absoluteTime, event) : events)
+
+midiToMidiSoundComposition ::
+  Midi.Track Midi.Ticks ->
+  Composition MidiSound
+midiToMidiSoundComposition track =
+  let noteDurations :: [((Rational, Rational), MidiNote)]
+      noteDurations = snd $ foldl noteDurationsFolder (Map.empty :: NoteStartMap, []) track
+      noteDurationsFolder ::
+        (NoteStartMap, [((Rational, Rational), MidiNote)]) ->
+        (Midi.Ticks, Midi.Message) ->
+        (NoteStartMap, [((Rational, Rational), MidiNote)])
+
+      noteDurationsFolder (noteState, notes) (when, Midi.NoteOff _channel _key _velocity) =
+        let (startTime, velocity) = fromMaybe (0, 0) $ Map.lookup (fromIntegral _channel, fromIntegral _key) noteState
+         in (Map.delete (fromIntegral _channel, fromIntegral _key) noteState, ((startTime, fromIntegral when), MidiNote {channel = fromIntegral _channel, note = fromIntegral _key, velocity = velocity}) : notes)
+      noteDurationsFolder (noteState, notes) (when, Midi.NoteOn _channel _key 0) =
+        let (startTime, velocity) = fromMaybe (0, 0) $ Map.lookup (fromIntegral _channel, fromIntegral _key) noteState
+         in (Map.delete (fromIntegral _channel, fromIntegral _key) noteState, ((startTime, fromIntegral when), MidiNote {channel = fromIntegral _channel, note = fromIntegral _key, velocity = fromIntegral velocity}) : notes)
+      noteDurationsFolder (noteState, notes) (when, Midi.NoteOn _channel _key _velocity) =
+        (Map.insert (fromIntegral _channel, fromIntegral _key) (fromIntegral when, fromIntegral _velocity) noteState, notes)
+      noteDurationsFolder (noteState, notes) (when, _event) = (noteState, notes)
+
+      treeMapper :: ((Rational, Rational), MidiNote) -> (Interval Rational, MidiSound)
+      treeMapper ((start, end), midiNote) = (Finite start <=..<= Finite end, Map.singleton (channel midiNote, note midiNote) (velocity midiNote))
+      tree :: SegmentTree.STree MidiSound Rational
+      tree = SegmentTree.fromList (treeMapper <$> noteDurations)
+   in fromMaybe Map.empty <$> queryTree tree
 
 sustain :: MidiNote -> Rational -> Composition MidiSound
 sustain midiNote dur t =
   if t < dur
-    then Map.singleton (channel midiNote) (Map.singleton (note midiNote) (velocity midiNote))
+    then Map.singleton (channel midiNote, note midiNote) (velocity midiNote)
     else Map.empty
 
 loopAt :: Rational -> Composition a -> Composition a
@@ -75,49 +116,60 @@ prettyPrintRational r =
 
 main :: IO ()
 main = do
-  putStrLn "Samples:"
-  let notes = sample (1 % 64) 4 (loopAt (1 % 1) (sustain (MidiNote 0 0 0) (1 % 2)))
-  forM_ notes $ \(t, audio) -> do
-    putStrLn $ prettyPrintRational t ++ ": " ++ show audio
+  eMidi <- Midi.importFile "cs1-1pre.mid"
+  case eMidi of
+    Left s -> putStrLn $ "Error: " ++ s
+    Right mi -> do
+      putStrLn ""
+      let track = Midi.tracks mi !! 1
+          trackAbsolute = trackAbsoluteTime track
+          composition = midiToMidiSoundComposition trackAbsolute
+          composition2 = \t -> composition t `mappend` ((`div` 2) <$> composition (fromIntegral endTime - t))
+          endTime = maximum $ map fst $ trackAbsolute
+      let notes = sample 1 (fromIntegral $ fst $ last trackAbsolute) composition2
 
-  putStrLn ""
-  putStrLn "Events:"
+      putStrLn "Ticks per beat: "
+      print (Midi.timeDiv mi)
 
-  let events = samplesToEvents notes
-  forM_ events $ \(t, (note, event)) -> do
-    putStrLn $ prettyPrintRational t ++ ": " ++ show note ++ " " ++ show event
+      putStrLn "Absolute time track:"
+      forM_ (take 40 (drop 11 trackAbsolute)) $ \(when, message) -> do
+        putStrLn $ show when ++ " " ++ show message
+      putStrLn ""
 
-  let eventToMidiMessage :: (Note, NoteEvent) -> Midi.Message
-      eventToMidiMessage (note, NoteOn) = Midi.NoteOn 9 (fromIntegral note) 100
-      eventToMidiMessage (note, NoteOff) = Midi.NoteOff 9 (fromIntegral note) 0
+      putStrLn "What's happening at 1900"
+      print (composition 1900)
 
-  let midiEvents = map (second eventToMidiMessage) events
+      let events = samplesToEvents notes
 
-      midiEventsWithOffsets :: [(Rational, Midi.Message)] -> [(Int, Midi.Message)]
-      midiEventsWithOffsets [] = []
-      midiEventsWithOffsets xs = reverse $ snd $ foldl offset (0, []) xs
+      putStrLn "Events"
+      forM_ (take 40 events) $ \(t, (channel, note, velocity, event)) -> do
+        putStrLn $ prettyPrintRational t ++ ": " ++ show channel ++ " " ++ show note ++ " " ++ show velocity ++ " " ++ show event
+      putStrLn ""
 
-      offset :: (Int, [(Int, Midi.Message)]) -> (Rational, Midi.Message) -> (Int, [(Int, Midi.Message)])
-      offset (oldTime, message) (now, newMessage) =
-        let newTime = floor $ now * 24
-         in (newTime, (newTime - oldTime, newMessage) : message)
+      let eventToMidiMessage :: (Channel, Note, Velocity, NoteEvent) -> Midi.Message
+          eventToMidiMessage (channel, note, velocity, NoteOn) = Midi.NoteOn (fromIntegral channel) (fromIntegral note) (fromIntegral velocity)
+          eventToMidiMessage (channel, note, velocity, NoteOff) = Midi.NoteOff (fromIntegral channel) (fromIntegral note) (fromIntegral velocity)
 
-      track = midiEventsWithOffsets midiEvents
+      let midiEvents = map (second eventToMidiMessage) events
 
-  let _trackOne =
-        [ (0, Midi.NoteOn {Midi.channel = 0, Midi.key = 69, Midi.velocity = 100}),
-          (24, Midi.NoteOn {Midi.channel = 0, Midi.key = 71, Midi.velocity = 100}),
-          (24, Midi.NoteOn {Midi.channel = 0, Midi.key = 73, Midi.velocity = 100}),
-          (24, Midi.NoteOn {Midi.channel = 0, Midi.key = 74, Midi.velocity = 100})
-        ]
-      midiFile =
-        Midi.Midi
-          { Midi.fileType = Midi.MultiTrack,
-            Midi.timeDiv = Midi.TicksPerBeat 24,
-            Midi.tracks = [(0, Midi.ProgramChange 9 0) : track]
-          }
+          midiEventsWithOffsets :: [(Rational, Midi.Message)] -> [(Int, Midi.Message)]
+          midiEventsWithOffsets [] = []
+          midiEventsWithOffsets xs = reverse $ snd $ foldl offset (0, []) xs
 
-  putStrLn "track:"
-  forM_ track $ \(t, m) -> do
-    putStrLn $ show t ++ ": " ++ show m
-  Midi.exportFile "test.mid" midiFile
+          offset :: (Int, [(Int, Midi.Message)]) -> (Rational, Midi.Message) -> (Int, [(Int, Midi.Message)])
+          offset (oldTime, message) (now, newMessage) =
+            (floor now, (floor now - oldTime, newMessage) : message)
+
+          track = midiEventsWithOffsets midiEvents
+
+          midiFile =
+            Midi.Midi
+              { Midi.fileType = Midi.MultiTrack,
+                Midi.timeDiv = Midi.TicksPerBeat 480,
+                Midi.tracks = [(0, Midi.ProgramChange 0 1) : track]
+              }
+
+      -- putStrLn "track:"
+      -- forM_ track $ \(t, m) -> do
+      --   putStrLn $ show t ++ ": " ++ show m
+      Midi.exportFile "test.mid" midiFile
